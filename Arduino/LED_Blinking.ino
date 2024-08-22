bf10558ea0436c72c864740a007d373ba658fb78
Abu Salyh Muhammad Mussa <105313863+AbuSalyh@users.noreply.github.com>
2024-08-22 15:36:33 +0600

    Update LED_Blinking.ino

--- Arduino/LED_Blinking.ino
+++ Arduino/LED_Blinking.ino
@@ -1,20 +1,14 @@
-int pwm=11;
-void setup () {
-  pinMode (pwm, OUTPUT);
+int LEDpin = 13;
 
+void setup() 
+{
+pinMode(LEDpin, OUTPUT);
 }
-  // put your setup code here, to run once:
 
-void loop() {
-  // put your main code here, to run repeatedly:
-analogWrite (pwm,64);
+void loop() 
+{
+digitalWrite(LEDpin, HIGH);
 delay(500);
-analogWrite (pwm,127);
+digitalWrite(LEDpin, LOW);
 delay(500);
-analogWrite (pwm,191);
-delay(500);
-analogWrite (pwm,255);
-delay(500);
-analogWrite (pwm,0);
-delay(5000);
 }
